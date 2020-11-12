4b328341f7adf9b27d25c93ce1ba6e4603f3ef16
Graham Eckel <grahameckel@gmail.com>
2020-11-12 14:25:59 -0500

    included the L1 Norm

--- k_nearest_neighbour.py
+++ k_nearest_neighbour.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # Load data set and code labels as 0 = ’NO’, 1 = ’DH’, 2 = ’SL’
 labels = [b'NO', b'DH', b'SL']
-data = np.loadtxt('...column_3C.dat', converters={6: lambda s: labels.index(s)} )
+data = np.loadtxt('C:/Users/graha/Documents/1. Studies/ENGG/Assignment 2/Spine/column_3C.dat', converters={6: lambda s: labels.index(s)})
 
 # Separate features from labels
 x = data[:,0:6]
@@ -17,7 +17,7 @@ trainy = y[training_indices]
 testx = x[test_indices,:]
 testy = y[test_indices]
 
-###Creating the individual functions
+###Creating the individual functions to calculate L2 Norm
 def squared_dist(x,y):
     #sum of squared differences
     return np.sum(np.square(x-y))
@@ -39,17 +39,23 @@ test_predictions = [NN_classifier(testx[i,]) for i in range(len(testy))]
 print(test_predictions)
 
 
-###Putting them all together and outputting an array of classifications
-def NN_L2(trainx, trainy, testx):
-    IndexArray = []
+###Putting them all together, with the L1 Norm, and outputting an array of classifications
+def NN_LN(trainx, trainy, testx):
+    L1IndexArray = []
+    L2IndexArray = []
     for x in range(len(testx)):
-        Distance = [np.sum((testx[x,]-trainx[i,])**2) for i in range(len(trainy))]
-        Index = np.argmin(Distance)
-        Label = trainy[Index]
-        IndexArray.append(Label)
-    return IndexArray
+        L1Distance = [np.sum(abs((testx[x,]-trainx[i,]))) for i in range(len(trainy))]
+        L1Index = np.argmin(L1Distance)
+        L1Label = trainy[L1Index]
+        L1IndexArray.append(L1Label)
+        
+        L2Distance = [np.sum((testx[x,]-trainx[i,])**2) for i in range(len(trainy))]      
+        L2Index = np.argmin(L2Distance)        
+        L2Label = trainy[L2Index]        
+        L2IndexArray.append(L2Label)
+    return L1IndexArray, L2IndexArray
 
-testy_L2 = NN_L2(trainx, trainy, testx)
+testy = NN_LN(trainx, trainy, testx)
 
-#matches our baseline
-print(testy_L2)
+#L2 matches our baseline
+print(testy)
